4748af0545dc74f384a9a68a7feb1e981662e3aa
Vu Doan <victordoan1708@gmail.com>
2020-05-09 16:02:50 +0100

    CORRECT WORKFLOW, CURRENT ISSUES: CAN'T BREAK THE WHILE LOOP AUTOMATICALLY

--- language-buddy.py
+++ language-buddy.py
@@ -16,6 +16,7 @@ import speech_recognition as sr
 # so that it's easier than to have pytesseract translate image to text with live video
 import cv2 as cv
 import numpy as np
+import utlis
 # import selenium for web scraping in order to automate the translation on Google Translate
 # import selenium
 # from selenium import webdriver
@@ -49,9 +50,9 @@ langs_gg = 'vi+en+ko+zh+fr'
 # link to download: https://sites.google.com/a/chromium.org/chromedriver/downloads
 # PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
-
 #Sample rate is how often values are recorded 
 sample_rate = 48000
+
 #Chunk is like a buffer. It stores 2048 samples (bytes of data) 
 #here.  
 #it is advisable to use powers of 2 such as 1024 or 2048 
@@ -61,8 +62,14 @@ chunk_size = 2048
 # initialise recogniser
 r = sr.Recognizer()
 
+global result
+result = None
 
 def chooseDestLanguage():
+    # chooseDestL_label = tk.Label(root, text="Speak Your Desired Destination Language", bg='gray')
+    # chooseDestL_label.pack()
+    print("Speak Your Desired Destination Language")
+
     with sr.Microphone(sample_rate = sample_rate,  
                         chunk_size = chunk_size) as source:
         # r.adjust_for_ambient_noise(source)
@@ -102,9 +109,9 @@ def chooseDestLanguage():
             elif 'hun' in destL:
                 destL = 'hu'
                 print(destL)
-            else:
-                warning_label = tk.Label(root, text="Apparently, we don't know that language yet", bg='gray')
-                warning_label.pack()
+            # else:
+            #     warning_label = tk.Label(root, text="Apparently, we don't know that language yet", bg='gray')
+            #     warning_label.pack()
             # call the function to do web scraping on Google Translate
             # googleTranslate()
             # call the function to do image-to-text analysis
@@ -117,70 +124,71 @@ def chooseDestLanguage():
 
 # initialise OPENCV live video webcam
 cap = cv.VideoCapture(0)
-
-def liveVideoCapture():
+widthImg = 960
+heightImg = 540
+webcamFeed = True
+path_to_imgFile =  'media/img/myImage.png'
+# initialise the system workflow with live camera
+def init(): 
     while True:
-        # loop through the img sequence and pass it to another variable
-        _, frames = cap.read()
-        gray = cv.cvtColor(frames, cv.COLOR_BGR2GRAY)
-        gray = np.float32(gray)
+        if webcamFeed:
+            _, frame = cap.read()
+        else:
+            frame = cv.imread(path_to_imgFile)
+        frame = cv.resize(frame, (widthImg, heightImg))
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        bilateral = cv.bilateralFilter(gray, 5, 12, 12)
+        canny = cv.Canny(bilateral, 100, 200)
 
-        corners = cv.goodFeaturesToTrack(gray, 50, 0.01, 10)
-        corners = np.int0(corners)
+        kernel = np.ones((5, 5))
+        dilate = cv.dilate(canny, kernel, iterations=2)
+        erode = cv.erode(dilate, kernel, iterations=1)
 
-        for c in corners:
-            x, y = c.ravel()
-            cv.circle(frames, (x, y), 3, 255, -1)
-        cv.imshow('Live Webcam', frames)
+        imgContour = frame.copy()
+        imgBigContour = frame.copy()
+        contours, _ = cv.findContours(erode, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        cv.drawContours(imgContour, contours, -1, (0, 255, 0), 10)
 
-        # end the loop if there is a key interrupts 
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+        # find the biggest contour
+        # global biggest, imgWarpColored, img_show
+        biggest, _ = utlis.biggestContour(contours)
+        if biggest.size != 0:
+            # reorder the list of the biggest resolution
+            biggest = utlis.reorder(biggest)
+            cv.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)
+            imgBigContour = utlis.drawRectangle(imgBigContour, biggest, 2)
 
-        # toText = pt.image_to_string(gray, lang=langs_tesseract)
-        # if toText == '' or toText == None:
-        #     pass
-        # else:
-        #     print(toText)
+            # warp perspective
+            pts1 = np.float32(biggest)
+            pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+            matrix = cv.getPerspectiveTransform(pts1, pts2)
+            imgWarpColored = cv.warpPerspective(frame, matrix, (widthImg, heightImg))
 
-# SELENIUM - WEB SCRAPING (GOOGLE TRANSLATE)
-# def googleTranslate():
-#     global driver
-#     # opts = ChromeOptions()
-#     # opts.add_experimental_option("excludeSwitches", ['enable-automation'])
-#     # choose the web browser type to be driven
-#     driver = webdriver.Chrome(PATH)
+            # remove 20px from each side
+            # grayscale the warped img
+            imgWarpGray = cv.cvtColor(imgWarpColored, cv.COLOR_BGR2GRAY)
+            imgAdaptiveThre = cv.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
+            imgAdaptiveThre = cv.bitwise_not(imgAdaptiveThre)
+            imgAdaptiveThre = cv.medianBlur(imgAdaptiveThre, 3)
 
-#     # specify the url to be driven, format it with the custom destL
-#     driver.get(f'https://translate.google.com/#view=home&op=translate&sl=auto&tl={destL}')
+            # show the biggest contour found in a live video
+            img_show = imgBigContour
+        else:
+            img_show = frame
 
-#     # set input field
-#     inputField = 'source'
-
-#     # set output field
-#     outputField = "//div[@class='result tlid-copy-target']"
-
-#     # find input field
-#     input = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(inputField))
-
-#     # input = driver.find_element_by_xpath("//textarea[@id='source']")
-#     # analyseImg()
-
-#     # wait for 3 secs for analysing text
-#     time.sleep(2)
-
-#     # input some keywords into the field
-#     input.send_keys(keywords)
-
-#     # wait for 3 secs
-#     time.sleep(3)
-
-#     # before finding out the output HTML element 
-#     # as it's hidden before the input field is filled with text
-#     output = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(outputField))
-
-#     print(output.text)
-#     time.sleep(3)
+        # either show the biggest contour found in a frame
+        # or just show the live video as usual
+        cv.imshow('Live Webcam', img_show)
+        if cv.waitKey(1) & 0xFF == ord('s'):
+            if biggest.size != 0:
+                cv.imwrite(path_to_imgFile, imgWarpColored)
+            else:
+                cv.imwrite(path_to_imgFile, img_show)
+            cv.waitKey(300)
+            chooseDestLanguage()
+            # break
+    cap.release()
+    cv.destroyAllWindows()
 
 # remember to add the json file to the environment PATH before running this code file
 def translateText(text):
@@ -195,13 +203,10 @@ def translateText(text):
 def analyseImg():
     # for prototyping
     # testing the accuracy in image-to-text analysis between grayscaled img and coloured img 
-    img = cv.imread('media/img/fra.png')
+    img = cv.imread(path_to_imgFile)
     # img = Image.open('media/img/korean.jpg')
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     bilateral = cv.bilateralFilter(gray, 5, 12, 12)
-    # threshold = cv.adaptiveThreshold(bilateral, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 9, 11)
-    # cv.imshow('img', bilateral)
-    # cv.imshow('Coloured', img)
     global keywords
     keywords = pt.image_to_string(bilateral, lang=langs_tesseract)
     cv.waitKey(0)
@@ -213,32 +218,38 @@ def analyseImg():
 def listenOrg():
     # listenField = driver.find_element_by_xpath("//div[@class='src-tts left-positioned ttsbutton jfk-button-flat source-or-target-footer-button jfk-button']")
     # listenField.click()
-    sourceL = result['detectedSourceLanguage']
-    path_to_audioFile = 'media/audio/input.mp3'
-    input = gTTS(text=keywords, lang=sourceL, slow=False)
-    input.save(path_to_audioFile)
-    os.system(f'start {path_to_audioFile}')
+    if result != None:
+        sourceL = result['detectedSourceLanguage']
+        path_to_audioFile = 'media/audio/input.mp3'
+        input = gTTS(text=keywords, lang=sourceL, slow=False)
+        input.save(path_to_audioFile)
+        os.system(f'start {path_to_audioFile}')
+    else:
+        print('You Need To Specify The Source Language First')
 
 def listenTrans():
     # listenField = driver.find_element_by_xpath("//div[@class='res-tts ttsbutton-res left-positioned ttsbutton jfk-button-flat source-or-target-footer-button jfk-button']")
     # listenField.click()
-    translated_text = result['translatedText']
-    path_to_audioFile = 'media/audio/output.mp3'
-    output = gTTS(text=translated_text, lang=destL, slow=False)
-    output.save(path_to_audioFile)
-    os.system(f'start {path_to_audioFile}')
-    
+    if result != None:
+        translated_text = result['translatedText']
+        path_to_audioFile = 'media/audio/output.mp3'
+        output = gTTS(text=translated_text, lang=destL, slow=False)
+        output.save(path_to_audioFile)
+        os.system(f'start {path_to_audioFile}')
+    else:
+        print('You Need To Specify The Source Language First')
+        
 # TKINTER
 root = tk.Tk()
 
 canvas = tk.Canvas(root, bg='white')
 canvas.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-webcamBtn = tk.Button(root, text='Open Live Webcam', padx=20, pady=10, fg='white', bg='black', command=liveVideoCapture)
-webcamBtn.pack()
+# webcamBtn = tk.Button(root, text='Open Live Webcam', padx=20, pady=10, fg='white', bg='black', command=liveVideoCapture)
+# webcamBtn.pack()
 
-imageRecognitionBtn = tk.Button(root, text='PyTesseract', padx=20, pady=10, fg='white', bg='black', command=analyseImg)
-imageRecognitionBtn.pack()
+# imageRecognitionBtn = tk.Button(root, text='PyTesseract', padx=20, pady=10, fg='white', bg='black', command=analyseImg)
+# imageRecognitionBtn.pack()
 
 speakBtn = tk.Button(root, text='Speak Your Desired Destination Language', padx=20, pady=10, fg='white', bg='black', command=chooseDestLanguage)
 speakBtn.pack()
@@ -249,4 +260,20 @@ listenOrgBtn.pack()
 listenTransBtn = tk.Button(root, text='Listen to The Translated Text', padx=20, pady=10, fg='white', bg='red', command=listenTrans)
 listenTransBtn.pack()
 
+resetBtn = tk.Button(root, text='Reset', padx=20, pady=10, fg='white', bg='red', command=init)
+resetBtn.pack()
+
 root.mainloop()
+
+################################################################################
+    # automatically initialises live webcam
+    # then waits for a button press for a still img captured from live video webcam
+    # then immediately asks users to speak a desired destination language once the above condition is satisfied
+    # this feature can either be triggered by a button OR by following this workflow
+    # ideally, users should follow the sysmte workflow to get the best result
+    # then automatically analyses the img, once a targeted language is choosen
+    # then automatically processes to the translation pipeline
+    # after that, users can either choose to listen to the original text language or the translated one
+    # then the whole workflow will be restarted once users hit the reset button
+
+################################################################################
