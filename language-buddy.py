814d7cf5a2abbd56f9193e9692eacc507b097549
Vu Doan <victordoan1708@gmail.com>
2020-05-14 21:02:02 +0100

    NAMED THE SYSTEM. Geolocation and Direction On Progress

--- language-buddy.py
+++ language-buddy.py
@@ -34,13 +34,16 @@ from playsound import playsound
 
 # import google-cloud-translate
 from google.cloud import translate_v2 as translate
+# import googlemaps for geolocations and directions
+import googlemaps
+from datetime import datetime
 
 # import wolframalpha for common knowledge
 import wolframalpha
 
 # import tkinter
 import tkinter as tk
-# print('Package Loaded')
+import random
 
 # specifying languages to be used for image-to-text analysis
 # vietnamese, english, korean, chinese simplified, french, african, finish, italian, japanese, hungarian, spanish, thai, russian, hindi, german
@@ -61,6 +64,8 @@ chunk_size = 2048
 # initialise recogniser
 r = sr.Recognizer()
 r1 = sr.Recognizer()
+r_maps = sr.Recognizer()
+r_greeting = sr.Recognizer()
 global result
 result = None
 
@@ -217,11 +222,11 @@ def init():
             cv.imshow('Live Webcam', img_show)
 
             if cv.waitKey(1) & 0xFF == ord('s'):
-                saveLabel = tk.Label(root, text='A Frame Saved' + '\n' + 'You Now Have Two Options: Choose A Destination Language' 
-                                    + '\n' + 'or Search Image On The Internet', bg='gray')
+                saveLabel = tk.Label(root, text='A Frame Saved' + '\n' + 'You Now Have 3 Options: Choose A Destination Language' 
+                                    + '\n' + 'Search Image On The Internet'
+                                    + '\n' + 'Or Terminate This Process', bg='gray')
                 saveLabel.pack()
                 path_to_audioFile = 'media/audio/response/after_img_capturing.mp3'
-                
                 if biggest.size != 0:
                     cv.imwrite(path_to_imgFile, imgWarpColored)
                 else:
@@ -241,15 +246,15 @@ def init():
         cap.release()
         cv.destroyAllWindows()
 
-def imageCaptured():
-    while True:
-        if cv.waitKey(1) & 0xFF == ord('s'):
-            if biggest.size != 0:
-                cv.imwrite(path_to_imgFile, imgWarpColored)
-            else:
-                cv.imwrite(path_to_imgFile, img_show)
-            cv.waitKey(300)
-            break
+# def imageCaptured():
+#     while True:
+#         if cv.waitKey(1) & 0xFF == ord('s'):
+#             if biggest.size != 0:
+#                 cv.imwrite(path_to_imgFile, imgWarpColored)
+#             else:
+#                 cv.imwrite(path_to_imgFile, img_show)
+#             cv.waitKey(300)
+#             break
 
 # remember to add the json file to the environment PATH before running this code file
 def translateText(text):
@@ -317,7 +322,7 @@ def imageDetection():
     time.sleep(0.75)
     # locate the img
     pyautogui.typewrite(['down', 'enter'])
-    time.sleep(0.75)
+    time.sleep(1)
 
     # google image search
     pyautogui.click(960, 640, button='right')
@@ -327,10 +332,18 @@ def imageDetection():
     path_to_audioFile_for_img_search = 'media/audio/response/after_img_search.mp3'
     playsound(path_to_audioFile_for_img_search)
 
+    # machine reply
+    path_to_audioFile_for_screenshot = 'media/audio/response/prep_for_screenshot.mp3'
+    playsound(path_to_audioFile_for_screenshot)
+
     # wait for webpage buffering
     time.sleep(8)
     im = pyautogui.screenshot(region=(440, 429, 331, 42))
     im.save(path_for_img_detection)
+
+    # machine reply
+    path_to_audioFile_for_img_analysed = 'media/audio/response/after_screenshot.mp3'
+    playsound(path_to_audioFile_for_img_analysed)
 
     # add label
     chooseDestL_label = tk.Label(root, text="Now, Choose A Destination Language", bg='gray')
@@ -339,6 +352,27 @@ def imageDetection():
     # machine reply
     path_to_audioFile_for_dest_lang = 'media/audio/response/choose_dest_lang.mp3'
     playsound(path_to_audioFile_for_dest_lang)
+
+def geolocate():
+    # provide api key for google maps access
+    API_KEY = 'AIzaSyDcjgYK5VK0QpJNG960wnfOOjXJUXYicac'
+    gmaps = googlemaps.Client(key=API_KEY)
+
+    # voice command for finding directions
+    with sr.Microphone(sample_rate = sample_rate,  
+                    chunk_size = chunk_size) as source:
+        r_maps.pause_threshold = 1
+        r_maps.adjust_for_ambient_noise(source, duration=0.5)
+        audio = r_maps.listen(source)
+        try:
+            directions = r_maps.recognize_google(audio)
+            # if ''
+            print(directions)
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
 
 def speakCommand():
     with sr.Microphone(sample_rate = sample_rate,  
@@ -379,8 +413,15 @@ def speakCommand():
             elif 'image' in SpeakCmd:
                 mode = 2
                 imageDetection()
-            
-            # if mode == 1:
+
+            # voice command for directions
+            if 'dir' in SpeakCmd:
+                mode = 3
+                geolocate()
+            elif 'find' in SpeakCmd:
+                mode = 3
+                geolocate()
+
             # voice command to listen to the original text
             if 'listen' in SpeakCmd:
                 if 'source' in SpeakCmd:
@@ -402,34 +443,64 @@ def speakCommand():
                     print(SpeakCmd)
                     listenTrans()
             
-            # if mode == 1 or mode == 2:
-            # voice command to choose a desired destination language
-            if 'choose' in SpeakCmd:
-                if 'dest'  in SpeakCmd:
-                    print(SpeakCmd)
-                    chooseDestLanguage()
-                elif 'lang' in SpeakCmd:
-                    print(SpeakCmd)
-                    chooseDestLanguage()
-            elif 'select' in SpeakCmd:
-                if 'dest'  in SpeakCmd:
-                    print(SpeakCmd)
-                    chooseDestLanguage()
-                elif 'lang' in SpeakCmd:
-                    print(SpeakCmd)
-                    chooseDestLanguage()
-            elif 'say' in SpeakCmd:
-                if 'dest'  in SpeakCmd:
-                    print(SpeakCmd)
-                    chooseDestLanguage()
-                elif 'lang' in SpeakCmd:
-                    print(SpeakCmd)
-                    chooseDestLanguage()
+            if mode == 1 or mode == 2:
+                # voice command to terminate tasks
+                if 'termin' in SpeakCmd:
+                    mode = 0
+                elif 'end' in SpeakCmd:
+                    mode = 0
+                
+                # voice command to choose a desired destination language
+                if 'choose' in SpeakCmd:
+                    if 'dest'  in SpeakCmd:
+                        print(SpeakCmd)
+                        chooseDestLanguage()
+                    elif 'lang' in SpeakCmd:
+                        print(SpeakCmd)
+                        chooseDestLanguage()
+                elif 'select' in SpeakCmd:
+                    if 'dest'  in SpeakCmd:
+                        print(SpeakCmd)
+                        chooseDestLanguage()
+                    elif 'lang' in SpeakCmd:
+                        print(SpeakCmd)
+                        chooseDestLanguage()
+                elif 'say' in SpeakCmd:
+                    if 'dest'  in SpeakCmd:
+                        print(SpeakCmd)
+                        chooseDestLanguage()
+                    elif 'lang' in SpeakCmd:
+                        print(SpeakCmd)
+                        chooseDestLanguage()
         except sr.UnknownValueError:
             print("Could not understand audio")
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
             
+def greeting():
+    hour = str(datetime.now()).split(' ')[1].split(':')[0]
+    if int(hour) < 12:
+        path_to_audioFile = 'media/audio/response/Good_Morning.mp3'
+    elif int(hour) >= 12 and int(hour) <= 18:
+        path_to_audioFile = 'media/audio/response/Good_Afternoon.mp3'
+    elif int(hour) > 18 and int(hour) <= 24:
+        path_to_audioFile = 'media/audio/response/Good_Day.mp3'
+    path_to_audioFile2 = 'media/audio/response/Yes_Sir.mp3'
+    greetings = [path_to_audioFile, path_to_audioFile2]
+    with sr.Microphone(sample_rate = sample_rate,  
+                    chunk_size = chunk_size) as source:
+        r_greeting.pause_threshold = 1
+        r_greeting.adjust_for_ambient_noise(source, duration=0.5)
+        audio = r_greeting.listen(source)
+        try:
+            greetAurora = r_greeting.recognize_google(audio)
+            if 'Aurora' in greetAurora:
+                playsound(random.choice(greetings))
+                speakCommand()
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
 
 # TKINTER
 canvas = tk.Canvas(root, bg='white')
@@ -437,6 +508,9 @@ canvas.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 # webcamBtn = tk.Button(root, text='Open Live Webcam', padx=20, pady=10, fg='white', bg='black', command=liveVideoCapture)
 # webcamBtn.pack()
+
+greetingsBtn = tk.Button(root, text='Greet Aurora', padx=20, pady=10, fg='white', bg='red', command=greeting)
+greetingsBtn.pack()
 
 speakBtn = tk.Button(root, text='Speak Command', padx=20, pady=10, fg='white', bg='black', command=speakCommand)
 speakBtn.pack()
@@ -461,7 +535,7 @@ root.mainloop()
     # then waits for a button press for a still img captured from live video webcam
     # then immediately asks users to speak a desired destination language once the above condition is satisfied
     # this feature can either be triggered by a button OR by following this workflow
-    # ideally, users should follow the sysmte workflow to get the best result
+    # ideally, users should follow the system workflow to get the best result
     # then automatically analyses the img, once a targeted language is choosen
     # then automatically processes to the translation pipeline
     # after that, users can either choose to listen to the original text language or the translated one
